35769d69d0577afd4fcc99abcf3ff5ddd0879f36
Islombek <whoisit864@gmail.com>
2023-07-02 10:47:28 +0300

    statusWindow

---
+++ code/statusWindow/statusWindow/statusWindowApp.swift
@@ -0,0 +1,17 @@
+//
+//  statusWindowApp.swift
+//  statusWindow
+//
+//  Created by Islombek Gofurov on 02.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct statusWindowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
