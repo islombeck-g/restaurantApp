a59d1259c915889500abb28ed499cb021f50c269
Islombek <whoisit864@gmail.com>
2023-07-02 10:44:47 +0300

    firstCodeCommit

---
+++ code/adminApp/adminApp/adminAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  adminAppApp.swift
+//  adminApp
+//
+//  Created by Islombek Gofurov on 02.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct adminAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
