c8c0230350943ed5a8e96efc39aef2b526fe5da2
Islombek <whoisit864@gmail.com>
2024-01-30 15:22:02 +0300

    remove some lines of code

--- code/RestaurantApp(3.0)/RestaurantApp(3.0)/EmployeesModule/Network/EmployeeService.swift
+++ code/RestaurantApp(3.0)/RestaurantApp(3.0)/EmployeesModule/Network/EmployeeService.swift
@@ -1,6 +1,5 @@
 import Foundation
 
-
 final class EmployeesService {
     
     let employeesAPI = EmployeeAPI()
@@ -25,47 +24,3 @@ final class EmployeesService {
     }
     
 }
-
-//
-//import Foundation
-//
-//
-//final class EmployeesService {
-//    
-//    let employeesAPI = EmployeeAPI()
-//    
-//    func getData(completion: @escaping ([EmployeeStruct])-> Void) {
-//        self.employeesAPI.get { employees in
-//            completion(employees)
-//        }
-//    }
-//    
-//    @MainActor
-//    func addToFirebase(employee: EmployeeStruct, completion: @escaping (String) -> Void) {
-//        guard employee.name != "" && employee.email != "" && employee.phone != "" && employee.position != "" else {
-//            completion("error in decode data")
-//            return
-//        }
-//        self.employeesAPI.add(employee: employee) { result in
-//            completion(result)
-//        }
-//    }
-//    
-//    func deleteFromFirebase(employeeID: String, completion: @escaping (String)-> Void) {
-//        guard employeeID != "" else {
-//            completion("employeer id is Empty")
-//            return
-//        }
-//        self.employeesAPI.delete(employeeID: employeeID) { result in
-//            completion(result)
-//        }
-//    }
-//    
-//    func improveDeleteFromFirebase(employeeID: String) -> EmployeeAPIResults {
-//        guard employeeID.isEmpty else { return .idIsEmpty }
-//        
-//        return self.employeesAPI.improveDelete(employeeID: employeeID)
-//    }
-//    
-//}
-//
