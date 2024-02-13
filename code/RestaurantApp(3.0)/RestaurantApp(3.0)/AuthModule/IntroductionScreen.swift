eb8a9bbbeda722a1b68dd3522ee822d7f6f37621
Islombek <whoisit864@gmail.com>
2024-02-13 14:37:21 +0300

    intro view
    add image

--- code/RestaurantApp(3.0)/RestaurantApp(3.0)/AuthModule/IntroductionScreen.swift
+++ code/RestaurantApp(3.0)/RestaurantApp(3.0)/AuthModule/IntroductionScreen.swift
@@ -3,20 +3,35 @@ import SwiftUI
 struct IntroductionScreen: View {
     
     @EnvironmentObject private var authViewModel:AuthViewModel
-
+    
     var body: some View {
         NavigationStack {
-            Text("Hi guys, it should be interesting")
-            Image("Bowl Icon")
-            NavigationLink {
-                AuthScreen()
-                    .environmentObject(authViewModel)
-            } label: {
-                Text("go to auth")
+            ZStack {
+                GeometryReader { geometry in
+                    Image("intro_0")
+                        .resizable()
+                        .scaledToFill()
+                    
+                    VStack {
+                        Text("It will be interesting \nlet's start")
+                            .styleMainText_25_black()
+                            .multilineTextAlignment(.center)
+                            .foregroundStyle(.white)
+                            .position(x: geometry.size.width/2, y: geometry.size.height/2)
+                        
+                        NavigationLink {
+                            AuthScreen()
+                                .environmentObject(authViewModel)
+                        } label: {
+                            Text("Go to auth")
+                                .foregroundStyle(.white)
+                                .styleMainText_20_black()
+                        }
+                        .position(x: geometry.size.width/2, y: geometry.size.height/2.5)
+                    }
+                }
             }
-
-            Image("Chef Hat Icon")
-                .frame(maxWidth: .infinity, alignment: .leading)
+            .ignoresSafeArea()
         }
     }
 }
